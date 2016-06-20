9862436937359f919da3ec7a2fcdebc0112e89da
tibbi <tibor@kaputa.sk>
2016-06-20 14:59:38 +0200

    lets also redirect to the list of apps

--- assets/redirect.js
+++ assets/redirect.js
@@ -1,7 +1,11 @@
 function checkRedirect(url) {
     if(navigator.userAgent.toLowerCase().indexOf("android") > -1) {
-        var pkgName = url.substring(url.lastIndexOf('=') + 1);
-        window.location.href= "market://details?id=" + pkgName;
+        if (url) {
+            var pkgName = url.substring(url.lastIndexOf('=') + 1);
+            window.location.href = "market://details?id=" + pkgName;
+        } else {
+            window.location.href = "market://search?q=pub:Simple+Mobile+Tools";
+        }
         return false;
     }
     return true;
