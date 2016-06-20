7d1d5815146a522518bcf3a485f990de2ce34002
tibbi <tibor@kaputa.sk>
2016-06-20 14:40:04 +0200

    open the playstore links in the app if clicked from Android

---
+++ assets/redirect.js
@@ -0,0 +1,9 @@
+function checkRedirect(url) {
+    if(navigator.userAgent.toLowerCase().indexOf("android") > -1) {
+        var pkgName = url.substring(url.lastIndexOf('=') + 1);
+        window.location.href= "market://details?id=" + pkgName;
+        return false;
+    }
+    return true;
+}
+
