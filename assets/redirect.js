99376cebd4d26f4b17c3d8a794caa0aab63ff8f8
tibbi <tibor@kaputa.sk>
2016-12-09 23:43:51 +0100

    update the developer page

--- assets/redirect.js
+++ assets/redirect.js
@@ -4,7 +4,7 @@ function checkRedirect(url) {
             var pkgName = url.substring(url.lastIndexOf('=') + 1);
             window.location.href = "market://details?id=" + pkgName;
         } else {
-            window.location.href = "market://search?q=pub:Simple+Mobile+Tools";
+            window.location.href = "https://play.google.com/store/apps/dev?id=9070296388022589266";
         }
         return false;
     }
